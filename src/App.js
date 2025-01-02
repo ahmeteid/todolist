479a3df850e3e73d88cb7e7bc3a77896e200148b
ahmeteid <ahmeteid355@gmail.com>
2025-01-02 15:59:33 +0300

    project complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ToDoForm from "./components/ToDoForm";
+import Todo from "./components/Todo";
 
 function App() {
+  let [todos, setTodos] = useState([]);
+  const [todoToShow, setTodoToShow] = useState("all");
+  const [toggleAllComplete, setToggleAllComplete] = useState(true);
+
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const updateTodoToShow = (show) => {
+    setTodoToShow(show);
+  };
+
+  if (todoToShow === "active") {
+    todos = todos.filter((todo) => !todo.complete);
+  } else if (todoToShow === "complete") {
+    todos = todos.filter((todo) => todo.complete);
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  const removeAllTodosThatAreCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.complete));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <ToDoForm onSubmit={addTodo} />
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            onDelete={() => handleDelete(todo.id)}
+            toggleComplete={() => toggleComplete(todo.id)}
+          />
+        ))}
+
+        <div>
+          <button
+            className="update-btn btn"
+            onClick={() => updateTodoToShow("all")}
+          >
+            All
+          </button>
+          <button
+            className="update-btn btn"
+            onClick={() => updateTodoToShow("active")}
+          >
+            Active
+          </button>
+          <button
+            className="update-btn btn"
+            onClick={() => updateTodoToShow("complete")}
+          >
+            Complete
+          </button>
+        </div>
+        {todos.some((todo) => todo.complete) ? (
+          <button
+            className="all-btn btn"
+            onClick={() => removeAllTodosThatAreCompleted()}
+          >
+            Remove All Complete Todos
+          </button>
+        ) : null}
+        <button
+          className="all-btn btn"
+          onClick={() => {
+            setTodos(
+              todos.map((todo) => ({
+                ...todo,
+                complete: toggleAllComplete,
+              }))
+            );
+            setToggleAllComplete(!toggleComplete);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Toggle All Complete: {`${toggleAllComplete}`}
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+function Todo(props) {
+  return (
+    <>
+      <div className="d-f">
+        <div
+          style={{ textDecoration: props.todo.complete ? "line-through" : "" }}
+          onClick={props.toggleComplete}
+        >
+          {props.todo.text}
+        </div>
+        <button className="delete-btn" onClick={props.onDelete}>
+          X
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Todo;
